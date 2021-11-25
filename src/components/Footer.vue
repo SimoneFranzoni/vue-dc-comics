9400c8f33a3d86ccab99b50fef4bf89f6de4e3e9
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-25 18:52:15 +0100

    Complete footer top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,99 @@
 <template>
-  
+    <footer>
+        <div class="footer-top">
+            <div class="container">
+                <div>
+                    <ul>
+                        <ol>Dc comics</ol>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <ol>Shop</ol>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <ol>Dc</ol>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <ol>Sites</ol>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>  
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bot">
+            <div class="container">
+                
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -9,5 +103,36 @@ export default {
 </script>
 
 <style>
+    .footer-top{
+        background-image: url("../assets/img/footer-bg.jpg");
+    }
+
+    .footer-top .container{
+        display: flex;
+    }
+
+    ul{
+        color: white;
+        list-style: none;
+        padding: 15px 50px 0 0;
+    }
+
+    ol{
+        font-weight: bold;
+        font-size: 22px;
+        text-transform: uppercase;
+        padding-bottom: 10px;
+    }
+
+    li a{
+        display: block;
+        text-decoration: none;
+        color: grey;
+    }
+
+    li a:hover{
+        color: white;
+
+    }
 
 </style>
