c106758fecdb8cc6084536dd1e8ba40704d27658
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-25 16:52:34 +0100

    commit di prova

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,15 @@
 <template>
-  
+    <main>
+        <div class="container container-sup">
+            <h1>Content goes here</h1>
+        </div>
+        <div class="container container-inf">
+            <div class="box">
+                <img src="..\assets\img\buy-comics-digital-comics.png" alt="">
+                <div>Digital Comics</div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -8,6 +18,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    
+    .container-sup{
+        background-color: #1c1c1c;
+        color: white;
+        padding: 50px;
+    }
+
+    .container-inf{
+        background-color: #0282f9;
+        padding: 50px 5%;
+        color: white;
+    }
+
+    .contaienr
 
 </style>
