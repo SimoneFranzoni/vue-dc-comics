ddeef86716db8ee79a30c4e2e264f725385ba35a
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-25 13:17:29 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,93 @@
+<template>
+  <header>
+        <div class="container">
+            <div>
+                <img src="../assets/img/dc-logo.png" alt="logo">
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="">Characters</a>
+                    </li>
+                    <li>
+                        <a href="">Comics</a>
+                    </li>
+                    <li>
+                        <a href="">MOvies</a>
+                    </li>
+                    <li>
+                        <a href="">Tv</a>
+                    </li>
+                    <li>
+                        <a href="">Games</a>
+                    </li>
+                    <li>
+                        <a href="">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="">Video</a>
+                    </li>
+                    <li>
+                        <a href="">Fans</a>
+                    </li>
+                    <li>
+                        <a href="">News</a>
+                    </li>
+                    <li>
+                        <a href="">Shop</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    header{
+        width: 100%;
+        background-color: white;
+        position: fixed;
+    }
+
+    .container{
+        width: 70%;
+        margin: 0 15%;
+        display: flex;
+        justify-content: space-between;
+        align-content: center; 
+    }
+
+    .container div{
+        display: flex;
+        align-items: center;
+    }
+
+    ul{
+        list-style: none;
+    }
+
+    li{
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: black;
+        text-align: center;
+        padding: 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        color: blue;
+        border-bottom: 1px solid blue;
+        /*il bordo spinge il blocco più in alto*/    
+    }
+
+</style>
